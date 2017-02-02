80dfb797d34bd0915c72c87d8afad28353badab3
2alexf68 <2alexf68@solent.ac.uk>
2017-02-02 13:05:36 +0000

    Layout to be reviewed

--- app/src/main/java/com/example/a2alexf68/mapping/HelloMap.java
+++ app/src/main/java/com/example/a2alexf68/mapping/HelloMap.java
@@ -7,8 +7,15 @@ import android.preference.PreferenceManager;
 import org.osmdroid.views.MapView;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.config.Configuration;
+import android.view.View.OnClickListener;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class HelloMap extends Activity
+import static java.lang.Integer.*;
+
+
+public class HelloMap extends Activity implements OnClickListener
 {
 
     MapView mv;
@@ -17,7 +24,6 @@ public class HelloMap extends Activity
     {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
         // this line tells OpenStreetMap about our app.
         // If you miss this out, you might get banned from OSM servers
@@ -27,7 +33,25 @@ public class HelloMap extends Activity
         mv = (MapView)findViewById(R.id.map1);
 
         mv.setBuiltInZoomControls(true);
-        mv.getController().setZoom(14);
+        mv.getController().setZoom(5);
         mv.getController().setCenter(new GeoPoint(40.1,22.5));
+
+        Button b = (Button) findViewById(R.id.locationButton);
+        b.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        EditText LatitudeEditText = (EditText)findViewById(R.id.LatitudeEditText);
+        String Latitude1AsString = LatitudeEditText.getText().toString();
+
+        EditText LongitudeEditText = (EditText)findViewById(R.id.LongitudeEditText);
+        String Longitude1AsString = LongitudeEditText.getText().toString();
+
+        double Latitude1 = Double.parseDouble(Latitude1AsString);
+        double Longitude1 =  Double.parseDouble(Longitude1AsString);
+
+        mv.getController().setCenter(new GeoPoint(Longitude1, Latitude1));
     }
 }
