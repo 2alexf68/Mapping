713bf96778988ac3b1af7344c8a017370f904e8f
2alexf68 <2alexf68@solent.ac.uk>
2017-02-09 11:00:01 +0000

    android map menu

--- app/src/main/java/com/example/a2alexf68/mapping/HelloMap.java
+++ app/src/main/java/com/example/a2alexf68/mapping/HelloMap.java
@@ -2,11 +2,16 @@ package com.example.a2alexf68.mapping;
 
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import org.osmdroid.views.MapView;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.config.Configuration;
+
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View.OnClickListener;
 import android.view.View;
 import android.widget.Button;
@@ -24,6 +29,8 @@ public class HelloMap extends Activity implements OnClickListener
     {
 
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
 
         // this line tells OpenStreetMap about our app.
         // If you miss this out, you might get banned from OSM servers
@@ -49,9 +56,34 @@ public class HelloMap extends Activity implements OnClickListener
         EditText LongitudeEditText = (EditText)findViewById(R.id.LongitudeEditText);
         String Longitude1AsString = LongitudeEditText.getText().toString();
 
-        double Latitude1 = Double.parseDouble(Latitude1AsString);
-        double Longitude1 =  Double.parseDouble(Longitude1AsString);
+        if(LongitudeEditText.equals("") & LatitudeEditText.equals(""))
+        {
+            mv.getController().setCenter(new GeoPoint (40.1,22.5));
+            mv.getController().setZoom(4);
+        } else {
+            double Latitude1 = Double.parseDouble(Latitude1AsString);
+            double Longitude1 = Double.parseDouble(Longitude1AsString);
 
-        mv.getController().setCenter(new GeoPoint(Longitude1, Latitude1));
+            mv.getController().setCenter(new GeoPoint(Longitude1, Latitude1));
+        }
+
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu_hello_map, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(item.getItemId() == R.id.choosemap)
+        {
+            Intent intent = new Intent(this,MapChooseActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return false;
     }
 }
